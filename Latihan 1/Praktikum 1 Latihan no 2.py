db4fdc796f40c8ddf9f754c5476ab534da4016c3
Kahfi Al Azhar <kahfialazhar09@gmail.com>
2020-11-08 14:51:00 +0700

    Initial commit

---
+++ Latihan 1/Praktikum 1 Latihan no 2.py
@@ -0,0 +1,24 @@
+# program hitung syarat kelulusan
+
+# input nilai
+
+# Bahasa Indonesia
+BahasaIndonesia = int(input('Nilai Bahasa Indonesia :'))
+if(BahasaIndonesia < 0 or BahasaIndonesia > 100):
+  print('Maaf input ada yang tidak valid')
+    
+# Matematika
+Matematika = int(input('Nilai Matematika :'))
+if(Matematika < 0 or Matematika > 100):
+     print('Maaf input ada yang tidak valid')
+
+# IPA
+IPA = int(input('Nilai IPA :'))
+if(IPA < 0 or IPA > 100):
+    print('Maaf input ada yang tidak valid')
+
+# Status Kelulusan
+if(BahasaIndonesia > 60 and IPA > 60 and Matematika > 70):
+  print('Status Kelulusan : LULUS')
+else:
+  print('Status Kelulusan : TIDAK LULUS')
---
+++ Latihan 2/Praktikum 2 latihan no 3.py
@@ -0,0 +1,10 @@
+i = 1
+sum = 0
+suku = 0
+while(i <= 100):
+ print(i)
+ i = i + 2
+ sum = sum + 1
+ suku = suku + i
+print('Banyaknya bilangan ganjil : ' + str(sum))
+print('Jumlah seluruh bilangan :' + str(suku))
